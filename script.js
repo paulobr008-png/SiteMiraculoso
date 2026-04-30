5be54d18e61f85c608f5048d4eedcd16a020ffa0
PauloBR00i <paulobr008@gmail.com>
2026-04-30 16:55:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,192 @@
+const S6Episodes = {
+    revelator: {
+        title: "REVELATOR",
+        video: "https://drive.google.com/file/d/1uxI0JRsH6uhaFeS7unighLDgJ_ARNjZf/preview",
+        desc: `Marinette e Adrien, o casal mais famoso de Paris,
+                atraem a atenção de um influenciador inescrupuloso que fará de tudo
+                por repercussão… até mesmo destruir o amor entre eles.`,
+        clip1: "https://drive.google.com/file/d/11rm47b6Jlu12CZ8USOzNi_uuiGKkuDbS/preview",
+        clip2: "https://drive.google.com/file/d/14Nq85vZusZaUPEkfaEz8EIrT6dy4gCrQ/preview",
+        clip3: "https://drive.google.com/file/d/1DEVOqylkzA3Ti6OaqtwQJBbiIGsxBxxl/preview",
+        clip1Title: "O Plano de Alya",
+        clip2Title: "Akumatização de Revelador",
+        clip1Icon: "https://drive.google.com/thumbnail?id=1UeczV-F8IgIUE2hSiDdI8MQTcX1Hei1w",
+        clip2Icon: "https://drive.google.com/thumbnail?id=1KmZgjnrVWxIgr8M-VjcTGdnnNaCYmFm4",
+        clip3Icon: "https://drive.google.com/thumbnail?id=1AWhKsTCItB5J-9GhK4Lm0gAvyzJeTRbO"
+    },
+
+    yaksiGozen: {
+        title: "YAKSI GOZEN",
+        video: "https://drive.google.com/file/d/117kur5AuGxHmUjg43Spn_YbfNP9IGPpf/preview",
+        desc: `Enquanto Adrien tenta descobrir como ser tão surpreendente
+                quanto Marinette, Kagami e Félix tentam descobrir como fugir das surpresas desagradáveis.`,
+        clip1: "https://drive.google.com/file/d/1UtSUavK6sSFVG5mLGE5rGb_K8pUe8n_-/preview",
+        clip2: "https://drive.google.com/file/d/1-iBV9eXBslmZ1AjuNay7LeqXMPFED8r8/preview",
+        clip3: "https://drive.google.com/file/d/1Tnu6DO1Lc9-KAz8J8C4AJ6caT9YZNq_x/preview",
+        clip1Title: "O Plano de Chrysalis",
+        clip2Title: "Akumatização de Yaksi Iozen",
+        clip1Icon: "https://drive.google.com/thumbnail?id=1mfGPVjCCczBy7o1Cc7iAjUfg72KaRk7g",
+        clip2Icon: "https://drive.google.com/thumbnail?id=1Z2Kt9UcBG1rF985IEeURG4mPevAB1h3B",
+        clip3Icon: "https://drive.google.com/thumbnail?id=1xcQPSYVBZ9OjWqK89ZDvkOvXJHaGWEbv"
+    },
+
+    riginarazione: {
+        title: "RIGINARAZIONE",
+        video: "https://drive.google.com/file/d/1wH6VuVg1Hq8YpH38vZNK_H3_5skTRH9j/preview",
+        desc: `Com o aniversário de 75 anos de seus avós, Marinette recebe
+                uma notícia que mudará completmamente a vida dela e de sua faília.`,
+        clip1: "https://drive.google.com/file/d/1ljaTRWMYoRk2WE3nmWxSk1oWhDKDmpcU/preview",
+        clip2: "https://drive.google.com/file/d/1Ej-xi-IdmJZZWz3jyh-GhkTNQZRDnutW/preview",
+        clip3: "https://drive.google.com/file/d/1vm3kl2DyH5D50MOMzTwfqRD9pVW7l7V4/preview",
+        clip1Title: "Marinette e Seu Avô",
+        clip2Title: "Akumatização de Rigina Razione",
+        clip1Icon: "https://drive.google.com/thumbnail?id=1ruLOCf8WRWchQRZ296yMOOLAbD7AtPXK",
+        clip2Icon: "https://drive.google.com/thumbnail?id=1qbuNmKQTvfLkQe45-5MEcyvzYErApXuk",
+        clip3Icon: "https://drive.google.com/thumbnail?id=1B0nkOcO07lpFAHt4RYMRBNWwhFygaY6_"
+    },
+
+    heartfixer: {
+        title: "RENVERSE-CŒURS",
+        video: "https://drive.google.com/file/d/1IC2bzOZFILIwf4HUriBa4bsPxgnWj7lL/preview",
+        desc: `Determinados a destruir tudo que remete ao amor, Nelson, Loïc e Ray
+                decidem se akumatizar para acabar com o relacionamento de Marinette e Adrien.`,
+        clip1: "https://drive.google.com/file/d/1tfEmq2dm-1TAvu1C2WhcWAuf_gZ1Hsd5/preview",
+        clip2: "https://drive.google.com/file/d/1b5pGtBRRqtBa8JF3QUi9XM3td4RhICTx/preview",
+        clip3: "https://drive.google.com/file/d/1jv9h9Vwtp2VbCuzMiPH_dMl8NGmYW2Fr/preview",
+        clip1Title: "Fim de Adrinette",
+        clip2Title: "Akumatização de Conserta-Corações",
+        clip1Icon: "https://drive.google.com/thumbnail?id=1HZndJ5CA11hs464VBmgJp6Me62unDx6k",
+        clip2Icon: "https://drive.google.com/thumbnail?id=1134YREYtFDY1b0c4tAuxR0b4CkGM3NLO",
+        clip3Icon: "https://drive.google.com/thumbnail?id=1QhpkXzROhZ3d75I-3-4N8b2hI1nQazsO"
+    },
+
+    theChainedTitans: {
+        title: "LES TITANS CHAÎNES",
+        video: "https://drive.google.com/file/d/1tbBN2tvpRva5EDlvSh_1S1PJnY9x1pzI/preview",
+        desc: `Nino sugere à Alya de substituir Carapace e Rena Rouge para proteger
+                Adrien e Marinette. Mas a chegada de dois supervilões complica tudo: como
+                podem Marinette e Adrien serem os substitutos de seus amigos e Ladybug e
+                Cat Noir ao mesmo tempo?`,
+        clip1: "https://drive.google.com/file/d/1mvO3Fe0dKs2l81xZgnmUTqRUUjpw685s/preview",
+        clip2: "https://drive.google.com/file/d/1QcSOQmJ0N2Kbvj-PZzt_YZNPmgPCAm4m/preview",
+        clip3: "https://drive.google.com/file/d/1lzKTCIm1qW9JGJl9jzm8KYjS3r1uM58E/preview",
+        clip1Title: "Renardette & Trionyx",
+        clip2Title: "Akumatização dos Titãs Gigantes",
+        clip1Icon: "https://drive.google.com/thumbnail?id=1dxNS18AhTyxuY2FUbyjJeJU1Pjamj5KM",
+        clip2Icon: "https://drive.google.com/thumbnail?id=1Rr5g6W5QwY2s35CikhbZEWK5uGN8e9gU",
+        clip3Icon: "https://drive.google.com/thumbnail?id=19YjGGVzhQIcmpser26pjti-YmJ8lRjEL"
+    },
+
+    sadnansi: {
+        title: "TRISTANANSI",
+        video: "https://drive.google.com/file/d/1bjXjovg_TEUTSztUe4n7IojY47pnX5LH/preview",
+        desc: `Convencida de que a irmã de Alya, Nora, e seu colega Twig
+                têm sentimentos um pelo outro, mas são tímidos demais para admitir,
+                Marinette decide assumir o papel de cupido.`,
+        clip1: "https://drive.google.com/file/d/1b6VzNrlZjan70EFv_71guiQOxyuyxdhO/preview",
+        clip2: "https://drive.google.com/file/d/1UJeVCugAX3LRlCvqO91QGB2D4vr0k_Gp/preview",
+        clip3: "https://drive.google.com/file/d/1Z878pB2mrg6qEAY_A9Ybo4jN52mExpSt/preview",
+        clip1Title: "O Desenho de Gabriel Agreste",
+        clip2Title: "Akumatização de Frianansi",
+        clip1Icon: "https://drive.google.com/thumbnail?id=19RKno6ExYNuYGsPa77HQlFCNJuELMRCz",
+        clip2Icon: "https://drive.google.com/thumbnail?id=131x_jtilsRh44drga6MTFNXoP9D7vdV9",
+        clip3Icon: "https://drive.google.com/thumbnail?id=11fAqMfRlv0K1cs86x43aQYCx4GFYuVhF"
+    }
+}
+
+const Specials = {
+    london: {
+        title: "MIRACULOUS WORLD LONDRES",
+        video: "https://drive.google.com/file/d/1paJ7fj2hAnTEtaOopM-qLcS1GLz3ZUkL/preview",
+        desc: `Para salvar o futuro, Marinette, agora Chronobug, deve se unir
+                a Bunnyx para resolver o mistério de quem descobriu a identidade secreta de Ladybug.`,
+        clip1: "https://drive.google.com/file/d/1quBxZZRB40pBowGx2NcZaQbyaut8IV2h/preview",
+        clip2: "https://drive.google.com/file/d/1j6KpH1jQJAtj1SI5BIxXKS0iuPfsRFif/preview",
+        clip3: "https://drive.google.com/file/d/1A9hhI9zL9UWV8kZUll08iCgP-6IxF1RK/preview",
+        clip1Title: "Ladybug Mente Para o Mundo",
+        clip2Title: "Chronobug vs Timestalker",
+        clip1Icon: "https://drive.google.com/thumbnail?id=1m9eCzH_1IKntz0Rb8Yx8d-KiQbaYENtn",
+        clip2Icon: "https://drive.google.com/thumbnail?id=1FWRJ459yZC30dRlbxJkzm2ZNlzeyyWQa",
+        clip3Icon: "https://drive.google.com/thumbnail?id=12DCrZRTB7aU51x72WsshOnCHmMZcs-WS"
+    }
+}
+
+const url = new URL(window.location.href);
+const params = new URLSearchParams(url.search);
+
+const season = params.get("season");
+const ep = params.get("ep");
+const clip = params.get("clip");
+
+const clipImg1 = document.querySelector("#img1");
+const clipImg2 = document.querySelector("#img2");
+const clipImg3 = document.querySelector("#img3");
+
+const backButton = document.querySelector("[data-BackButton]");
+const title = document.querySelector("[data-EPtitle]")
+const video = document.querySelector("#video");
+const desc = document.querySelector("#desc");
+const descSection = document.querySelector("[data-EPDesc]");
+const clipsSection = document.querySelector("[data-Clips]");
+const clip1Title = document.querySelector("#clip1Title");
+const clip2Title = document.querySelector("#clip2Title");
+
+switch (season) {
+    case "6":
+        LoadEpisode(S6Episodes, "Seasons/Season6.html");
+        break;
+
+    case "special":
+        LoadEpisode(Specials, "index.html");
+        break;
+}
+
+function LoadEpisode(SeasonObject, hrefLink) {
+    for (const episode in SeasonObject) {
+        if (episode == ep) {
+            const selectedEP = SeasonObject[episode];
+            switch (parseInt(clip)) {
+                case 0:
+                    title.innerHTML = selectedEP.title;
+                    video.src = selectedEP.video;
+                    desc.innerHTML = selectedEP.desc;
+                    clip1Title.innerHTML = selectedEP.clip1Title;
+                    clip2Title.innerHTML = selectedEP.clip2Title;
+                    clipImg1.src = selectedEP.clip1Icon;
+                    clipImg2.src = selectedEP.clip2Icon;
+                    clipImg3.src = selectedEP.clip3Icon;
+                    clipImg1.parentElement.href = `episodePage.html?season=${season}&ep=${episode}&clip=1`;
+                    clipImg2.parentElement.href = `episodePage.html?season=${season}&ep=${episode}&clip=2`;
+                    clipImg3.parentElement.href = `episodePage.html?season=${season}&ep=${episode}&clip=3`;
+                    break;
+
+                case 1:
+                    title.innerHTML = `${selectedEP.title} - ${selectedEP.clip1Title.toUpperCase()}`;
+                    video.src = selectedEP.clip1;
+                    descSection.remove();
+                    clipsSection.remove();
+                    break;
+
+                case 2:
+                    title.innerHTML = `${selectedEP.title} - ${selectedEP.clip2Title.toUpperCase()}`;
+                    video.src = selectedEP.clip2;
+                    descSection.remove();
+                    clipsSection.remove();
+                    break;
+
+                case 3:
+                    title.innerHTML = `${selectedEP.title} - CENA PRÉ-CRÉDITOS`;
+                    video.src = selectedEP.clip3;
+                    descSection.remove();
+                    clipsSection.remove();
+                    break;
+            }
+            backButton.addEventListener("click", () => {
+                const link = backButton.parentElement;
+                if (parseInt(clip) === 0) {
+                    link.href = hrefLink;
+                } else link.href = `episodePage.html?season=${season}&ep=${episode}&clip=0`;
+            })
+        }
+    }
+}
